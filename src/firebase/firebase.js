c1849cabb868307fe70caafb5d530edb1cb25b49
Ferkze <ferkemartins@gmail.com>
2018-08-16 19:10:13 -0300

    First commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase"
+
+var config = {
+    apiKey: "AIzaSyA9K9xowmgAUJfzLQti1jIIONH9JfLImPc",
+    authDomain: "selecta-services.firebaseapp.com",
+    databaseURL: "https://selecta-services.firebaseio.com",
+    projectId: "selecta-services",
+    storageBucket: "selecta-services.appspot.com",
+    messagingSenderId: "746538168186"
+}
+
+firebase.initializeApp(config)
